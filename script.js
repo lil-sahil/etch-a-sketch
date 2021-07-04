35b7a5520620191d90683b57cd502ced21b15c7c
lil-sahil <sahilnagpal93@yahoo.ca>
2021-07-04 15:41:04 -0400

    Implemented drawing capabilities and reset button.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.querySelector('.container');
 const gridSize = [container.offsetWidth, container.offsetHeight];
 
-let squareSize = 40;
 
-let numberOfSquares =  ( gridSize[0] * gridSize[1] ) / ( squareSize * squareSize ) 
 
+let numberOfRows = 64; 
+let squareSize =  gridSize[0] / numberOfRows; 
 
 
 function createSquare() {
@@ -12,7 +12,7 @@ function createSquare() {
     const etchPadSquare = document.createElement('div');
     
     etchPadSquare.setAttribute("class", "square");
-    etchPadSquare.style.outline = 'red 1px solid';
+    // etchPadSquare.style.outline = 'red 1px solid';
     etchPadSquare.style.width = `${squareSize}px`;
     etchPadSquare.style.height = `${squareSize}px`;
 
@@ -21,13 +21,45 @@ function createSquare() {
 }    
 
 
-function specifyGrid () {
-    for (let i = 0; i < numberOfSquares; i++ ){
+function createGrid () {
+    for (let i = 0; i < (numberOfRows*numberOfRows); i++ ){
         container.appendChild(createSquare());
     }
 }
 
-specifyGrid();
+createGrid();
+
+
+// Hover Effect
+const square = document.querySelectorAll('.square');
+
+square.forEach(element => { 
+    element.addEventListener('mouseenter', e => {
+        e.target.style.backgroundColor = 'red'})
+});
+
+// Holding control while hovering should not draw anything (Not complete).
+
+square.forEach(element => {
+    element.addEventListener('onkeypress', (e)=>console.log(e.keyCode));
+
+});
+
+// Add an eraser button
+// ...
 
 
 
+
+
+
+// Button Clear
+const button = document.querySelector('.button-primary');
+
+button.addEventListener('click', () => reset());
+
+function reset(){
+    square.forEach(element => {
+        element.style.backgroundColor = 'white';
+    });
+}
